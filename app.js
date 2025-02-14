6fed4e81527e9abc5b21473cac57f8a58bd5e598
Jheyker13 <jheyker13@gmail.com>
2025-02-14 11:54:45 +0000

    ejercicio patricia

--- app.js
+++ app.js
@@ -1,25 +1,27 @@
 // window.onload para asegurarnos de que todo el html carga antes de ejecutar JavaScript
-window.onload = function () {
+window.onload = function(){
 
     // Inicializa con el primer mensaje
     let descripcion = document.getElementById("descripcionPersonaje");
-    descripcion.innerHTML = "Antes era esclavo de los Malfoy, pero Harry Potter lo libera en Harry Potter y la cámara secreta."; // Primer mensaje al cargar
-
+      descripcion.innerHTML= "Mago de sangre mestiza, sobeviviente de la maldición de Avada Kedavra,  amigo de Ron y hermione"         // Primer mensaje al cargar
+   
     // Accedemos al documento y luego obtenemos por #id y disparamos la ejecución de la función con el evento onclick
-    document.getElementById("botonCambiarTexto").onclick = function () {
+     document.getElementById("botonCambiarTexto").onclick = function(){
 
-        // Definimos los tres mensajes, cada uno en su variable (const porque no va a cambiar)
-        const mensaje1 = "Antes era esclavo de los Malfoy, pero Harry Potter lo libera en Harry Potter y la cámara secreta."; // Primer mensaje
-        const mensaje2 = "Dobby ama la libertad y los calcetines, ¡y es muy leal a sus amigos! ✨"; // Segundo mensaje
-        const mensaje3 = "¡Dobby está feliz de ser libre!✨"; // Tercer mensaje
+        // Definimos losn tres mensajes, cada uno en su variable (const porque no va a cambiar)
+         //const PrimerMensaje = "Un niño de 11 años que descubre que es hijo de magos famosos y que asistirá al Colegio Hogwarts de Magia y Hechicería"; // Primer mensaje
+         //const segundoMensaje = "Lucha para derrotar a Lord Voldemort"; // Segundo mensaje
+         //const tercerMensaje = "Poseedor final de las reliquias de la muerte"; // Tercer mensaje
 
-        // Condición para cambiar el mensaje
-        if (descripcion.innerHTML === mensaje1) {
-            descripcion.innerHTML = mensaje2; // Primer click muestra el segundo mensaje
-        } else if (descripcion.innerHTML === mensaje2) {
-            descripcion.innerHTML = mensaje3; // Segundo click muestra el tercer mensaje
-        } else {
-            descripcion.innerHTML = mensaje1; // Vuelve al primer mensaje
-        }
-    };
-};
+         let mensajes =["Un niño de 11 años que descubre que es hijo de magos famosos y que asistirá al Colegio Hogwarts de Magia y Hechicería" ,"Lucha para derrotar a Lord Voldemort","Poseedor final de las reliquias de la muerte",
+         ]
+
+         let random = Math.floor(Math.random() * mensajes.length)
+         
+        document.querySelector("#descripcionPersonaje").innerHTML= mensajes[random]
+         // Condición para cambiar el mensaje
+         // Primer click muestra el segundo mensaje
+        // Segundo click muestra el tercer mensaje
+        // Vuelve al primer mensaje
+    }
+}
